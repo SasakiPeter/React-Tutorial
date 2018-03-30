77522d808331fd76fb6066ee0339d0358e7c1a6f
SasakiPeter <anagentofthegod@gmail.com>
2018-03-30 14:15:37 +0900

    update

--- src/index.js
+++ src/index.js
@@ -49,14 +49,15 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
-        const squares = this.state.squares.slice();
+        const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
@@ -65,21 +66,29 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
-
+        //Arrayにmapかけると配列全てに適応される挙動を定義できる
         const moves = history.map((step, move) => {
             const desc = move ?
                 'Move #' + move :
                 'Game start';
             return (
-                <li>
+                <li key={move}>
                     <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
                 </li>
             );
